edd7b6ab8f4db0904ecd8b75cd37e909f5fdd4da
Laxmanan Krishnapillai <65386461+Laxmanan-Krishnapillai@users.noreply.github.com>
2022-08-20 14:46:11 +0200

    Fixing shit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "Lrq7QR7kuGkmjWAkJKNS7g==",
+      "VNGSuCeeB75fnJQvuylygw==",
+      "HwnoLHUYWG/9tbhRDWQilA==",
+      "eqV9Mo1ec0Vqj3b8vlV0qQ==",
+    ]),
   },
 });
